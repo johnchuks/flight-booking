7e178d0f1f08a21803a8dc09430270258d927ed9
Andela Developer <johnboscoohia@gmail.com>
2018-11-14 11:55:52 +0100

    add test case for ticketviewset

--- flight/tests/test_base.py
+++ flight/tests/test_base.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIRequestFactory
 from account.tests.factories import login_user, UserFactory
-from flight.api.views import FlightViewSet
+from flight.api.views import FlightViewSet, TicketViewSet
 from flight.tests.factories import FlightFactory, TicketFactory
 from flight.models import Flight, Ticket
 
@@ -263,3 +263,287 @@ class TestTicketViewSet(APITestCase):
         self.user_token = logged_in_user.data['token']
 
         self.factory = APIRequestFactory()
+
+        self.flight = FlightFactory(
+            flight_number="BA-3443",
+            arrival_location="london",
+            departure_location="lagos",
+            departure_time="12:30pm",
+            arrival_time="6:00am",
+            arrival_date="2015-11-05",
+            departure_date='2015-11-05',
+            price=30000
+        )
+
+
+    def test_list_ticket_success(self):
+        TicketFactory(
+            status=Ticket.CONFIRMED,
+            flight=self.flight,
+            user=self.user
+        )
+        TicketFactory(
+            status=Ticket.CONFIRMED,
+            flight=self.flight,
+            user=UserFactory()
+        )
+
+        url = reverse('tickets-list')
+        view = TicketViewSet.as_view(
+            actions={
+                'get': 'list'
+            }
+        )
+        request = self.factory.get(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.admin_token))
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+
+    def test_book_ticket_success(self):
+        ticket = TicketFactory(
+            status=Ticket.RESERVED,
+            flight=self.flight,
+            user=self.user
+        )
+        url = reverse('tickets-book', args=(ticket.pk,))
+
+        view = TicketViewSet.as_view(
+            actions={
+                'patch': 'book'
+            }
+        )
+        request = self.factory.patch(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+        response = view(request, pk=ticket.pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['status'], Ticket.BOOKED)
+
+    def test_book_ticket_unauthorized(self):
+        ticket = TicketFactory(
+            status=Ticket.BOOKED,
+            flight=self.flight,
+            user=self.user
+        )
+        url = reverse('tickets-book', args=(ticket.pk,))
+
+        view = TicketViewSet.as_view(
+            actions={
+                'patch': 'book'
+            }
+        )
+
+        request = self.factory.patch(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.admin_token))
+        response = view(request, pk=ticket.pk)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data['message'], "You are not authorized to book this ticket")
+
+    def test_book_ticket_with_status_booked(self):
+        ticket = TicketFactory(
+            status=Ticket.BOOKED,
+            flight=self.flight,
+            user=self.user
+        )
+        url = reverse('tickets-book', args=(ticket.pk,))
+
+        view = TicketViewSet.as_view(
+            actions={
+                'patch': 'book'
+            }
+        )
+
+        request = self.factory.patch(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+        response = view(request, pk=ticket.pk)
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data['message'], "This ticket has either been booked or purchased")
+
+    def test_purchase_ticket_success(self):
+        ticket = TicketFactory(
+            status=Ticket.BOOKED,
+            flight=self.flight,
+            user=self.user
+        )
+        url = reverse('tickets-purchase', args=(ticket.pk,))
+
+        view = TicketViewSet.as_view(
+            actions={
+                'post': 'purchase'
+            }
+        )
+        request = self.factory.post(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+        response = view(request, pk=ticket.pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['status'], Ticket.CONFIRMED)
+
+    def test_purchase_ticket_fail_unauthorized(self):
+        ticket = TicketFactory(
+            status=Ticket.BOOKED,
+            flight=self.flight,
+            user=self.user
+        )
+
+        url = reverse('tickets-purchase', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'post': 'purchase'
+            }
+        )
+        request = self.factory.post(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.admin_token))
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data['message'], "You are not authorized to purchase this ticket")
+
+    def test_purchase_ticket_fail_confirmed(self):
+        ticket = TicketFactory(
+            status=Ticket.CONFIRMED,
+            flight=self.flight,
+            user=self.user,
+            arrival_location="Benin"
+        )
+
+        url = reverse('tickets-purchase', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'post': 'purchase'
+            }
+        )
+        request = self.factory.post(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['message'], "Ticket has been purchased for this flight")
+
+
+    def get_ticket_by_id_success(self):
+        ticket = TicketFactory(
+            status=Ticket.CONFIRMED,
+            flight=self.flight,
+            user=self.user
+        )
+
+        url = reverse('tickets-detail', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'get': 'detail'
+            }
+        )
+        request = self.factory.get(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['arrival_location'], "Benin")
+
+    def get_ticket_by_id_unauthorized(self):
+        ticket = TicketFactory(
+            status=Ticket.CONFIRMED,
+            flight=self.flight,
+            user=self.user
+        )
+
+        url = reverse('tickets-detail', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'get': 'detail'
+            }
+        )
+        request = self.factory.get(url, HTTP_AUTHORIZATION='JWT {}'.format(
+            self.admin_token))
+
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['message'], 'Some of the fields provided are not permitted for this action')
+
+
+    def test_update_ticket_success(self):
+        ticket = TicketFactory(
+            status=Ticket.RESERVED,
+            flight=self.flight,
+            user=self.user
+        )
+
+        url = reverse('tickets-detail', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'put': 'update'
+            }
+        )
+        request = self.factory.put(url, data=dict(arrival_location="lagos"), HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['arrival_location'], "lagos")
+
+    def test_update_ticket_unauthorized_access(self):
+        ticket = TicketFactory(
+            status=Ticket.RESERVED,
+            flight=self.flight,
+            user=self.user
+        )
+
+        url = reverse('tickets-detail', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'put': 'update'
+            }
+        )
+        request = self.factory.put(url, data=dict(arrival_location="lagos"), HTTP_AUTHORIZATION='JWT {}'.format(
+            self.admin_token))
+
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data['message'], 'You are not authorized to update this ticket')
+
+    def test_update_ticket_fail_for_booked_and_confirmed(self):
+        ticket = TicketFactory(
+            status=Ticket.CONFIRMED,
+            flight=self.flight,
+            user=self.user
+        )
+
+        url = reverse('tickets-detail', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'put': 'update'
+            }
+        )
+        request = self.factory.put(url, data=dict(arrival_location="lagos"), HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['message'], "Cannot update a booked or confirmed ticket")
+
+
+    def test_update_ticket_fail_for_disallowed_field(self):
+        ticket = TicketFactory(
+            status=Ticket.RESERVED,
+            flight=self.flight,
+            user=self.user
+        )
+
+        url = reverse('tickets-detail', args=(ticket.pk,))
+        view = TicketViewSet.as_view(
+            actions={
+                'put': 'update'
+            }
+        )
+        request = self.factory.put(url, data=dict(status=Ticket.BOOKED), HTTP_AUTHORIZATION='JWT {}'.format(
+            self.user_token))
+
+        response = view(request, pk=ticket.pk)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['message'], "Some of the fields provided are not permitted for this action")
+
+
+
