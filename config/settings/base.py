ea86c67789dc7a0e1aace4f8e373227040bd4b5d
Andela Developer <johnboscoohia@gmail.com>
2018-11-19 23:53:31 +0100

    change staticfiles storage

--- config/settings/base.py
+++ config/settings/base.py
@@ -209,7 +209,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
