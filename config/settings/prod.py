4ed2cd34a1d2ec145bf4e2fedbb385de01e17ab6
Andela Developer <johnboscoohia@gmail.com>
2018-11-20 06:17:02 +0100

    change config for database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -12,10 +12,14 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 ## Database config
-DATABASES = {
- 'default': parse(os.getenv('DATABASE_URL'))
-}
-
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+# DATABASES = {
+#  'default': parse(os.getenv('DATABASE_URL'))
+# }
+#
+# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES = {}
+DATABASES['default'] = config(
+    default=os.getenv('DATABASE_URL')
+)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
